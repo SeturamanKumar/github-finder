7e4ef8e049db9b236c189b5936d250ce2aeeeb67
Seturaman Kumar <seturamankumar@gmail.com>
2025-08-17 14:15:05 +0530

    Feat: Implement GitHub API Call and display user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
 import React, { useState } from "react";
 import './App.css';
+import UserCard from "./UserCard";
+
+interface UserProfile{
+  avatar_url: string;
+  name: string;
+  login: string;
+  bio: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  html_url: string;
+}
 
 function App(){
 
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState<string>('');
+  const [userData, setUserData] = useState<UserProfile | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null)
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(event.target.value);
+  };
+
+  const handleSearch = async () => {
+
+    if(!username) { return; }
+
+    setLoading(true)
+    setUserData(null);
+    setError(null);
+
+    try {
+
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if(!response.ok){
+        throw new Error(`User Not Found`);
+      }
+      const data: UserProfile = await response.json();
+      setUserData(data);      
+
+    } catch (err) {
+
+      if(err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('AN Unknown Error Occured');
+      }
+
+    } finally {
+      setLoading(false);
+    }
+
   };
 
   return(
@@ -23,14 +69,17 @@ function App(){
             value={username}
             onChange={handleInputChange}
           />
-          <button className="search-button">Search</button>
+          <button className="search-button" onClick={handleSearch} disabled={loading}>
+            {loading ? 'Searching...' : 'Search'}
+          </button>
         </div>
         <div className="results-container">
-          {/*User profile card will go in here in a future step*/}
+          {error && <p className="error-message">{error}</p>}
+          {userData && <UserCard user={userData} />}
         </div>
       </main>
     </div>
-  )
+  );
 
 }
 
