44adb97a71d11b52fb2aa8dc439f8a7c67ab7fbb
Seturaman Kumar <seturamankumar@gmail.com>
2025-08-17 12:00:35 +0530

    Feat: Create Basic UI Layout For App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+function App(){
+
+  return(
+    <div className="app-container">
+      <header className="app-header">
+        <h1>GitHub User Finder</h1>
       </header>
+      <main>
+        <div className="search-container">
+          <input 
+            type="text"
+            className="search-input"
+            placeholder="Enter a GitHub username..."
+            />
+            <button className="search-button">Search</button>
+        </div>
+        <div className="results-container">
+          {/*User profile card will go in here in a future step*/}
+        </div>
+      </main>
     </div>
-  );
+  )
+
 }
 
 export default App;
